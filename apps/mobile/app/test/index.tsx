371b544028a7a34128751043a7638d4243372d80
ales2026 <ale.urankar@gmail.com>
2026-03-09 20:33:34 +0100

    configure mobile for api and testing it

--- apps/mobile/app/test/index.tsx
+++ apps/mobile/app/test/index.tsx
@@ -1,10 +1,22 @@
 import { View, Text, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { api } from "@packages/utils";
 
 const Test = () => {
+    const [feedMessage, setFeedMessage] = useState<string>("Loading...");
+
+    useEffect(() => {
+        // Test your API endpoint
+        api
+        .get("/test")
+        .then((res) => setFeedMessage(res.data.message || "API OK"))
+        .catch((err) => setFeedMessage("API error: " + err.message));
+    }, []);
 
     return (
         <View style={styles.container}>
-           <Text>This is Test Page</Text>
+            <Text style={styles.message}>{feedMessage}</Text>
+            <Text>This is Test Page</Text>
         </View> 
     );
 };
@@ -14,6 +26,11 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 20,
         backgroundColor: '#ffffff',
+    },
+        message: {
+        color: "red",
+        marginBottom: 20,
+        textAlign: "center",
     },
 });
 
