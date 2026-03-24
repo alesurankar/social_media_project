e7b90f07fcd23745d3efd99bc520202786dd2f5a
ales2026 <ale.urankar@gmail.com>
2026-03-24 12:42:18 +0100

    web: add leftSidebar control button

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -5,8 +5,9 @@ import { ReactNode, useState } from "react";
 import { Icon } from "@packages/icons";
 
 
-export default function AppLayout({ children }: { children: ReactNode }) {
+const AppLayout = ({ children }: { children: ReactNode }) => {
   const [rightSidebar, setRightSidebar] = useState(true);  
+  const [leftSidebar, setLeftSidebar] = useState(true);  
 
   return (
     <div className="flex flex-col h-screen">
@@ -16,15 +17,17 @@ export default function AppLayout({ children }: { children: ReactNode }) {
       <div className="flex flex-1 relative">
 
         {/* Left sideBar */}
-        <div className="w-[18%] border bg-[#fff]">
-          <h3>Right Sidebar</h3>
-        </div>
-
         <button 
-          className="absolute top-0 right-0 m-2 bg-transparent border-none z-50"
-          onClick={() => setRightSidebar(prev => !prev)}
+          className="absolute top-0 left-0 m-2 bg-transparent border-none z-50"
+          onClick={() => setLeftSidebar(prev => !prev)}
           ><Icon name="bars_arrow_down" size={32} color="black"/>
         </button>
+        
+        {leftSidebar && (
+          <div className="w-[18%] border bg-[#fff]">
+            <h3 className="pt-20">Left Sidebar</h3>
+          </div>
+        )}
 
         {/* Main content */}
         <div className="flex-1 border bg-[#ccc] overflow-auto">
@@ -32,12 +35,20 @@ export default function AppLayout({ children }: { children: ReactNode }) {
         </div>
 
         {/* Right sidebar */}
+        <button 
+          className="absolute top-0 right-0 m-2 bg-transparent border-none z-50"
+          onClick={() => setRightSidebar(prev => !prev)}
+          ><Icon name="bars_arrow_down" size={32} color="black"/>
+        </button>
+
         {rightSidebar && (
           <div className="absolute top-0 right-0 w-[18%] h-full border bg-[#fff]">
-            <h3>Left Sidebar</h3>
+            <h3 className="pt-20">Right Sidebar</h3>
           </div>
         )}
       </div>
     </div>
   );
 };
+
+export default AppLayout;
