6a2ad472c9b553341b7c1861baf11a6754b7bf81
ales2026 <ale.urankar@gmail.com>
2026-03-24 11:43:58 +0100

    test positions in testingPage

--- apps/mobile/components/TestingPage.tsx
+++ apps/mobile/components/TestingPage.tsx
@@ -6,32 +6,110 @@ const TestingPage = () => {
   const router = useRouter();
 
   return(
-    <View>
-      
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => router.push('/test')}
-      ><Text style={styles.buttonText}>Test</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+
+      {/* Left */}
+      <View style={styles.left}>
+        <View style={styles.leftTop}>
+          <Text style={styles.text}>L-T-1</Text>
+        </View>
+        <View style={styles.leftBottom}>
+          <Text style={styles.text}>L-B-1</Text>
+        </View>
+      </View>
+
+      {/* Right */}
+      <View style={styles.right}>
+
+        {/* Top */}
+        <View style={styles.rightTop}>
+          <Text style={styles.text}>R-T-1</Text>
+        </View>
+
+        {/* Bottom */}
+        <View style={styles.rightBottom}>
+          <View style={styles.rightBottomLeft}>
+            <Text style={styles.text}>R-B-1</Text>
+          </View>
+          <TouchableOpacity style={styles.rightBottomRight}
+            onPress={() => router.push('/test')}
+          ><Text style={styles.text}>Test</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
 
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#4d5c75',
-    paddingVertical: 12,
-    paddingHorizontal: 25,
-    borderRadius: 8,
-    alignItems: 'center',
-    width: '100%',
-    marginBottom: 10,
+  container: {
+    flex: 1,
+    flexDirection: 'row',
   },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
+  left: {
+    flexDirection: 'column',
+    backgroundColor: '#333',
+    borderWidth: 2,
+    borderColor: 'white',
+    width: '35%',
+  },
+  leftTop: {
+    flex: 1,
+    backgroundColor: '#222',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  leftBottom: {
+    height: '20%',
+    backgroundColor: '#222',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  right: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#111',
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  rightTop: {
+    flex: 1,
+    backgroundColor: '#111',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  rightBottom: {
+    flex: 1,
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  rightBottomLeft: {
+    flex: 1,
+    backgroundColor: '#333',
+    borderWidth: 1,
+    borderColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rightBottomRight: {
+    flex: 0.25,
+    backgroundColor: '#000',
+    borderWidth: 1,
+    borderColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 36,
   },
 });
 
--- apps/web/src/components/TestingPage.tsx
+++ apps/web/src/components/TestingPage.tsx
@@ -7,7 +7,7 @@ const TestingPage = () => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-row h-screen text-4xl">
+    <div className="flex text-white h-screen text-4xl">
 
       {/* Left */}
       <div className="flex flex-col w-[20%] bg-gray-200 border-2 border-white">
@@ -20,7 +20,7 @@ const TestingPage = () => {
       </div>
 
       {/* Right */}
-      <div className="flex flex-col flex-1 text-white border-2 border-white">
+      <div className="flex flex-col flex-1 border-2 border-white">
 
         {/* Top */}
         <div className="flex flex-1 bg-[#111] items-center justify-center border-2 border-white">
@@ -32,9 +32,9 @@ const TestingPage = () => {
           <div className="flex flex-1 bg-[#333] border-1 border-white items-center justify-center ">
             R-B-1
           </div>
-          <button className="flex w-[20%] bg-[#000] border-1 border-white items-center justify-center"
-            onClick={() => router.push("/test")}>
-            R-B-2
+          <button className="flex flex-[0.25] bg-[#000] text-white border-1 border-white items-center justify-center"
+            onClick={() => router.push("/test")}
+            >Test
           </button>
         </div>
       </div>
